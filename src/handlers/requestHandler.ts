d77af70d06ac85e11eb70fefa5aff69d974155e6
Aresiren <baiyouming2011@126.com>
2025-01-18 20:48:49 +0800

    Update requestHandler.ts
    添加ip白名单

--- src/handlers/requestHandler.ts
+++ src/handlers/requestHandler.ts
@@ -5,6 +5,24 @@ import { setCORSHeaders } from '../utils/cors';
 import { logger } from '../utils/logger';
 
 export async function handleRequest(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	const { BUCKET, BUCKET_NAME, IP_WHITELIST_SWITCH, IP_WHITELIST_REGEX } = env;  // 从 env 中获取 BUCKET 和 BUCKET_NAME
+	const url = new URL(request.url);
+	const originHostname = url.hostname;
+  if(IP_WHITELIST_SWITCH == 1){//是否开启 ip白名单：1-开启
+		if(IP_WHITELIST_REGEX &&
+          !new RegExp(IP_WHITELIST_REGEX).test(
+            request.headers.get("cf-connecting-ip")
+          )){
+				return URL302
+          ? Response.redirect(URL302, 302)
+          : new Response(await nginx(), {
+              headers: {
+                "Content-Type": "text/html; charset=utf-8",
+              },
+            });
+		}
+	}
+	
   try {
     if (request.method !== "OPTIONS" && !authenticate(request, env)) {
       return new Response("Unauthorized", {
@@ -24,4 +42,30 @@ export async function handleRequest(request: Request, env: Env, ctx: ExecutionCo
     logger.error("Error in request handling:", error);
     return new Response("Internal Server Error", { status: 500 });
   }
+}
+
+async function nginx() {
+  return `<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>`;
 }
